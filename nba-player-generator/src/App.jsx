66facde0a385044b22c06d2cebba89bc92d5e14a
Corey Scott <cfscott261@gmail.com>
2026-04-10 14:26:16 -0400

    updated player list

--- nba-player-generator/src/App.jsx
+++ nba-player-generator/src/App.jsx
@@ -1,12 +1,40 @@
+import { useState} from "react"
 import "./App.css"
 
 function App() {
+
+  const playerNames = [
+    "Michael Jordan",
+    "LeBron James",
+    "Kobe Bryant",
+    "Shaquille O'Neal",
+    "Magic Johnson",
+    "Larry Bird",
+    "Tim Duncan",
+    "Allen Iverson",
+    "Penny Hardaway",
+    "Kevin Durant",
+    "Stephen Curry",
+    "Kawhi Leonard",
+    "Kevin Garnett",
+    "Grant Hill",
+    "Tracy McGrady",
+    "Dirk Nowitzki",
+    "Chris Paul",
+    "Derrick Rose",
+    
+  ]
+const [player, setPlayer] = useState(null)
+const [loading, setLoading] = useState(true)
+const [error, setError] = useState("")
+
+
   return (
-    <div>
-      <h1>NBA Player Spotlight</h1>
-      <p>My app is starting!</p>
-    </div>
-  )
+  <div>
+    <h1>NBA Player Spotlight</h1>
+    <p>Total players: {playerNames.length}</p>
+  </div>
+)
 }
 
 export default App
